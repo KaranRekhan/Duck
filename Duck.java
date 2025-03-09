209815ad6bdd18972b9b53193f48671fd267beab
Karan Rekhan <159275147+KaranRekhan@users.noreply.github.com>
2025-03-09 09:33:20 +0530

    Provided setter methods to allow dynamic behavior changes at runtime.

--- Duck.java
+++ Duck.java
@@ -2,3 +2,15 @@ abstract public class Duck {
     QuackBehaviour quackBehaviour;
     SwimBehaviour swimBehaviour;
     FlyingBehaviour flyingBehaviour;
+    public void setQuackBehaviour(QuackBehaviour quackBehaviour) {
+        this.quackBehaviour = quackBehaviour;
+    }
+
+    public void setSwimBehaviour(SwimBehaviour swimBehaviour) {
+        this.swimBehaviour = swimBehaviour;
+    }
+
+    public void setFlyingBehaviour(FlyingBehaviour flyingBehaviour) {
+        this.flyingBehaviour = flyingBehaviour;
+    }
+
