b8467f000b95df1be3da3a4e486312fb2f691e77
Karan Rekhan <159275147+KaranRekhan@users.noreply.github.com>
2025-03-09 09:37:51 +0530

    Assigned specific behaviors:   - Quack() for quackBehaviour (Mallard ducks quack normally).   - SwimWithLegs() for swimBehaviour (Mallard ducks swim using legs).   - CanFly() for flyingBehaviour (Mallard ducks can fly).

--- MallardDuck.java
+++ MallardDuck.java
@@ -1,1 +1,6 @@
 public class MallardDuck extends Duck{
+    public MallardDuck(){
+        quackBehaviour=new Quack();
+        swimBehaviour=new SwimWithLegs();
+        flyingBehaviour=new CanFly();
+    }
