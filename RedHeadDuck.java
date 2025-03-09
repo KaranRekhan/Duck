1eff09931f31e57ec53624abe77ac9ee02204b75
Karan Rekhan <159275147+KaranRekhan@users.noreply.github.com>
2025-03-09 09:44:08 +0530

    Assigned specific behaviors:   - Quack() for quackBehaviour (RedHead ducks quack normally).   - SwimWithLegs() for swimBehaviour (RedHead ducks swim using legs).   - CanFly() for flyingBehaviour (RedHead ducks can fly).

--- RedHeadDuck.java
+++ RedHeadDuck.java
@@ -1,1 +1,7 @@
 public class RedHeadDuck extends Duck{
+
+    public RedHeadDuck(){
+        quackBehaviour=new Quack();
+        swimBehaviour=new SwimWithLegs();
+        flyingBehaviour=new CanFly();
+    }
