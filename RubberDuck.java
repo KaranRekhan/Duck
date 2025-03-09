4f3b69781a4bf08b9dcdb8554a359d2f6d5232f4
Karan Rekhan <159275147+KaranRekhan@users.noreply.github.com>
2025-03-09 09:44:49 +0530

    Assigned specific behaviors:   - Squeak() for quackBehaviour (Rubber ducks squeak instead of quacking).   - Floating() for swimBehaviour (Rubber ducks float on water).   - CannotFly() for flyingBehaviour (Rubber ducks cannot fly).

--- RubberDuck.java
+++ RubberDuck.java
@@ -1,2 +1,8 @@
 public class RubberDuck extends Duck {
 
+    public RubberDuck(){
+        quackBehaviour=new Squeak();
+        swimBehaviour=new Floating();
+        flyingBehaviour=new Cannotfly();
+    }
+
