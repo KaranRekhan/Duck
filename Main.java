2de1f0e723a2c476f9d84925edfabab471764295
Karan Rekhan <159275147+KaranRekhan@users.noreply.github.com>
2025-03-09 09:36:48 +0530

    Instantiated different types of ducks: MallardDuck, RubberDuck, RedHeadDuck, and DecoyDuck.

---
+++ Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        MallardDuck md= new MallardDuck();
+        RubberDuck rd= new RubberDuck();
+        RedHeadDuck rhd= new RedHeadDuck();
+        DecoyDuck dd = new DecoyDuck();
